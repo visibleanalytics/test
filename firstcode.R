f6d9e29c8830de061e2a0708e19c2efc2aa608ff
Rachel <rachel.evans@visibleanalytics.co.uk>
2020-02-28 15:04:41 +0000

    This is the first bit of code, saved to Egnyte and git!

---
+++ firstcode.R
@@ -0,0 +1,19 @@
+### test run for connecting Rstudio to GitHub 
+#https://happygitwithr.com/rstudio-git-github.html 
+
+# steps:
+
+# register  GitHub account 
+# install/update R and RStudio 
+#install Git (chapter 6).
+#introduce yourself to Git (chapter 7).
+#confirm that you can push to / pull from GitHub from the command line
+
+# tools > global options > check r knows the location of git
+# create new repo on github.com
+# file > new project > version control > git 
+# copy repo URL
+
+# make changes to code etc and save as usual along the way
+
+# once finished: top right panel: git tab - select 'staged' then commit 
